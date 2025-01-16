c2d241db2a7fc92db96d28367e3f238fc5a04815
akocoglu <ahmetkocoglu@outlook.com>
2025-01-16 18:01:11 +0300

    Initial commit

---
+++ src/main/java/ank/phito/erp/JavaSpringBootErpApplication.java
@@ -0,0 +1,13 @@
+package ank.phito.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ank/phito/erp/JavaSpringBootErpApplicationTests.java
@@ -0,0 +1,13 @@
+package ank.phito.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
